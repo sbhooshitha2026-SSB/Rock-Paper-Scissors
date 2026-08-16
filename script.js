a2c4f411724d83e4e2dbe7a74d61b975d82eadbb
Susara Sri Bhooshitha <293163588+sbhooshitha2026-SSB@users.noreply.github.com>
2026-08-16 14:40:12 +0530

    created and tested playRound() function

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-console.log("Hello World")
+
 
 let humanScore = 0
 let computerScore = 0
@@ -22,5 +22,32 @@ function getHumanChoice() {
     return prompt("Enter Rock, Paper, or Scissors","").toLowerCase()
 }
 
-console.log(getHumanChoice());
+function getHumanChoice1() {
+    let a = getHumanChoice();
+    return a[0].toUpperCase() + a.slice(1)
+}
+
+function playRound(humanChoice, computerChoice) {
+     humanChoice = getHumanChoice1();
+     computerChoice = getComputerChoice();
+
+    if (humanChoice === "Rock" && computerChoice === "Scissors") {
+        console.log("You Win! Rock beats Scissors");
+        humanScore = humanScore + 1
+    } else if (humanChoice === "Paper" && computerChoice === "Rock") {
+        console.log("You Win! Paper beats Rock");
+        humanScore = humanScore + 1
+    } else if (humanChoice === "Scissors" && computerChoice === "Paper") {
+        console.log("You Win! Scissors beat Paper");
+        humanScore = humanScore + 1
+    } else {
+        console.log("You lose! computerChoice beat humanChoice");
+        computerScore = computerScore + 1
+    }
+}
+
+playRound();
+
+
+
 
